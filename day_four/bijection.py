31ece8b9f15645f476b40f839c82e7b6f4457228
Sno0wFl4ke <beeadev@outlook.com>
2024-03-01 12:50:45 +0100

    feat: Added Bijection, thanks to Saqib

--- day_four/bijection.py
+++ day_four/bijection.py
@@ -1,55 +1,64 @@
-"""
-This doesn't function
-
 class Bijection:
     def __init__(self, other=None):
-        self.mapping = {}
-        self.inverse_mapping = {}
-
-        if other is not None:
-            if isinstance(other, Bijection):
-                self.mapping = dict(other.mapping)
-                self.inverse_mapping = dict(other.inverse_mapping)
-            elif isinstance(other, dict):
-                for key, value in other.items():
-                    self[key] = value
+        self._f = {}
+        self._inverse = {}
+        if isinstance(other, dict):
+            for key, value in other.items():
+                self[key] = value
+        elif isinstance(other, Bijection):
+            self._f = other._f.copy()
+            self._inverse = other._inverse.copy()
+        elif other is not None:
+            raise ValueError("Argument error")
 
     def __str__(self):
-        return "{" + ",".join(f"{key}->{value}" for key, value in self.mapping.items()) + "}"
+        pairs = [key + "->" + value for key, value in self._f.items()]
+        return "{" + ", ".join(pairs) + "}"
 
     def __getitem__(self, key):
-        return self.mapping.get(key, None)
+        if key in self._f:
+            return self._f[key]
+        return None
 
     def __setitem__(self, key, value):
-        if value in self.inverse_mapping and key != self.inverse_mapping[value]:
-            raise KeyError("duplicate value")
-        if key in self.mapping:
-            del self.inverse_mapping[self.mapping[key]]
-        self.mapping[key] = value
-        self.inverse_mapping[value] = key
+        if value in self._f.values() or value in self._inverse.values():
+            raise KeyError("Duplicate")
+
+        if key in self._f:
+            old_value = self._f[key]
+            del self._inverse[old_value]
+
+
+        if value in self._inverse:
+
+            o_key = self._inverse[value]
+            del self._f[o_key]
+
+        self._f[key] = value
+        self._inverse[value] = key
 
     def __delitem__(self, key):
-        if key in self.mapping:
-            value = self.mapping[key]
-            del self.mapping[key]
-            del self.inverse_mapping[value]
+        if key not in self._f:
+            raise KeyError("Key does not exist!")
+        value = self._f.pop(key)
+        del self._inverse[value]
 
     def __len__(self):
-        return len(self.mapping)
+        return len(self._f)
 
     def __mul__(self, other):
-        result = Bijection()
-        for key, value in self.mapping.items():
-            if value in other.inverse_mapping:
-                result[key] = other.inverse_mapping[value]
+        if not isinstance(other, Bijection):
+            raise ValueError("Value Error")
+
+        composition = Bijection()
+        for key, value in other._f.items():
+            if value in self._f:
+               composition[key] = self._f[value]
             else:
-                raise KeyError(f"missing key: {value} in {other}")
-        return result
+                raise KeyError(f"Missing key: {value}")
+        return composition
 
     def inverse(self):
-        result = Bijection()
-        result.mapping = dict(self.inverse_mapping)
-        result.inverse_mapping = dict(self.mapping)
-        return result
-
-"""
+        inversed = Bijection()
+        inversed._f, inversed._inverse = self._inverse.copy(), self._f.copy()
+        return inversed
